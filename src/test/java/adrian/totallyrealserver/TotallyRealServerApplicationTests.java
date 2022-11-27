5a743e19d70424b48fc45205e2bb818b6fe3fc50
Adrian Butler Jr <butlerjr123@gmail.com>
2022-11-27 11:03:03 -0600

    setup project

---
+++ src/test/java/adrian/totallyrealserver/TotallyRealServerApplicationTests.java
@@ -0,0 +1,15 @@
+package adrian.totallyrealserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotallyRealServerApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
