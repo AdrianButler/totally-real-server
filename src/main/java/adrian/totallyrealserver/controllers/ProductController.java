b010fbd65bc04ab5acfd33779132e0059fae679c
Adrian Butler Jr <butlerjr123@gmail.com>
2023-01-04 10:19:54 -0600

    Added featured product endpoint

--- src/main/java/adrian/totallyrealserver/controllers/ProductController.java
+++ src/main/java/adrian/totallyrealserver/controllers/ProductController.java
@@ -1,8 +1,26 @@
 package adrian.totallyrealserver.controllers;
 
-import org.springframework.stereotype.Controller;
+import adrian.totallyrealserver.models.Product;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ProductController
 {
+	@GetMapping("/featured-product")
+	public Product getFeaturedProducts()
+	{
+		final String name = "4090 Founders Edition";
+		final String description = "";
+		final double price = 1599.99;
+		final double rating = 4.8;
+		final List<String> images = new ArrayList<>();
+		images.add("https://i.ibb.co/d0tGkZc/4090fe.webp");
+
+		Product product = new Product(name, description, price, rating, images);
+
+		return product;
+	}
 }
