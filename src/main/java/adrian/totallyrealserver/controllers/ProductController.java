21895803c500ce1f96fdc797cec5dd503da61b6b
Adrian Butler Jr <butlerjr123@gmail.com>
2023-01-10 12:46:35 -0600

    Added getProduct end point

--- src/main/java/adrian/totallyrealserver/controllers/ProductController.java
+++ src/main/java/adrian/totallyrealserver/controllers/ProductController.java
@@ -1,26 +1,27 @@
 package adrian.totallyrealserver.controllers;
 
 import adrian.totallyrealserver.models.Product;
-import java.util.ArrayList;
-import java.util.List;
+import adrian.totallyrealserver.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController
 {
+	@Autowired
+	ProductRepository productRepository;
+
 	@GetMapping("/featured-product")
 	public Product getFeaturedProducts()
 	{
-		final String name = "4090 Founders Edition";
-		final String description = "";
-		final double price = 1599.99;
-		final double rating = 4.8;
-		final List<String> images = new ArrayList<>();
-		images.add("https://i.ibb.co/d0tGkZc/4090fe.webp");
+		return productRepository.findById(1L).get();
+	}
 
-		Product product = new Product(name, description, price, rating, images);
-
-		return product;
+	@GetMapping("/product/{id}")
+	public Product getProduct(@PathVariable Long id)
+	{
+		return productRepository.findById(id).get();
 	}
 }
