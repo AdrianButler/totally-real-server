86df51e5f31b6742da47f5a54d8e1a9125f4403f
Adrian Butler Jr <butlerjr123@gmail.com>
2023-03-01 22:01:41 -0600

    added email otp to login endpoints

--- src/main/java/adrian/totallyrealserver/controllers/AuthController.java
+++ src/main/java/adrian/totallyrealserver/controllers/AuthController.java
@@ -3,9 +3,14 @@ package adrian.totallyrealserver.controllers;
 import adrian.totallyrealserver.models.StoreUser;
 import adrian.totallyrealserver.repositories.StoreUserRepository;
 import adrian.totallyrealserver.services.EmailAuthService;
+import java.util.Date;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class AuthController
@@ -14,10 +19,13 @@ public class AuthController
 
 	final EmailAuthService emailAuthService;
 
-	public AuthController(StoreUserRepository storeUserRepository, EmailAuthService emailAuthService)
+	final PasswordEncoder passwordEncoder;
+
+	public AuthController(StoreUserRepository storeUserRepository, EmailAuthService emailAuthService, PasswordEncoder passwordEncoder)
 	{
 		this.storeUserRepository = storeUserRepository;
 		this.emailAuthService = emailAuthService;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@PostMapping("/signup")
@@ -37,8 +45,42 @@ public class AuthController
 	}
 
 	@PostMapping("/login")
-	public StoreUser loginUser(@RequestBody StoreUser user) // TODO implement a real auth system
+	public void loginUser(@RequestBody Map<String, String> requestBody)
 	{
-		return storeUserRepository.findStoreUserByEmail(user.getEmail());
+		// Send OTP email to user
+
+		final String email = requestBody.get("email");
+		emailAuthService.sendOneTimePassword(storeUserRepository.findStoreUserByEmail(email));
 	}
+
+	@PostMapping("/login/verify")
+	public StoreUser verifyLogin(@RequestBody Map<String, String> requestBody)
+	{
+		String email = requestBody.get("email");
+		String oneTimePassword = requestBody.get("oneTimePassword");
+
+		StoreUser storeUser = storeUserRepository.findStoreUserByEmail(email);
+
+		if (checkIfOtpExpired(storeUser.getOtpRequestDate()))
+		{
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "One time password has expired");
+		}
+
+		if (!passwordEncoder.matches(oneTimePassword, storeUser.getOneTimePassword()))
+		{
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "One time password was incorrect");
+		}
+
+		return storeUser;
+	}
+
+	private boolean checkIfOtpExpired(Date otpRequestDate)
+	{
+		final long OTP_DURATION = 300000; // five minutes
+
+		long otpExpiration = otpRequestDate.getTime() + OTP_DURATION;
+
+		return otpExpiration < new Date().getTime();
+	}
+
 }
