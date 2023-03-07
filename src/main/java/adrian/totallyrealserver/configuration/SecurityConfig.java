57f59dd8d92c2cc48d75312018c50eb29515e488
Adrian Butler Jr <butlerjr123@gmail.com>
2023-03-06 21:39:10 -0600

    implemented jwt filter and changed httpSecurity rules

--- src/main/java/adrian/totallyrealserver/configuration/SecurityConfig.java
+++ src/main/java/adrian/totallyrealserver/configuration/SecurityConfig.java
@@ -1,30 +1,43 @@
 package adrian.totallyrealserver.configuration;
 
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig
 {
+	private final Filter jwtAuthenticationFilter;
+
+	public SecurityConfig(Filter jwtAuthenticationFilter)
+	{
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception
 	{
-		return httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-						.requestMatchers("/auth/**")
-						.permitAll()
-						.anyRequest()
-						.authenticated())
-				.httpBasic()
+		return httpSecurity.authorizeHttpRequests()
+				.requestMatchers("/auth/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
 				.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 				.csrf()
 				.disable()
 				.build();
