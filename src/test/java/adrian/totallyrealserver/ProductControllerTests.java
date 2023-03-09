8388f7922510b85859e8ba853429869188f6d9ca
Adrian Butler Jr <butlerjr123@gmail.com>
2023-03-09 15:43:18 -0600

    added tests for 2 product endpoints

---
+++ src/test/java/adrian/totallyrealserver/ProductControllerTests.java
@@ -0,0 +1,56 @@
+package adrian.totallyrealserver;
+
+import adrian.totallyrealserver.models.Product;
+import adrian.totallyrealserver.repositories.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+//@Import(SecurityConfig.class)
+public class ProductControllerTests
+{
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	public void testGetFeaturedProduct() throws Exception
+	{
+		MvcResult results = mockMvc.perform(MockMvcRequestBuilders.get("/product/featured"))
+				.andReturn();
+		String responseBody = results.getResponse()
+				.getContentAsString();
+
+		Product recievedProduct = objectMapper.readValue(responseBody, Product.class);
+		Product expectedProduct = productRepository.findById(1L).get();
+
+		assertEquals(recievedProduct, expectedProduct);
+	}
+
+	@Test
+	public void testGetProduct() throws Exception
+	{
+		MvcResult results = mockMvc.perform(MockMvcRequestBuilders.get("/product/1"))
+				.andReturn();
+		String responseBody = results.getResponse()
+				.getContentAsString();
+
+		Product recievedProduct = objectMapper.readValue(responseBody, Product.class);
+		Product expectedProduct = productRepository.findById(1L).get();
+
+		assertEquals(recievedProduct, expectedProduct);
+	}
+}
