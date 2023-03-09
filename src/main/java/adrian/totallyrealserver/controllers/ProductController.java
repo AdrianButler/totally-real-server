bd551dc86ad9e2c77ce0ae1f1d50fe5b6ef62d61
Adrian Butler Jr <butlerjr123@gmail.com>
2023-03-09 15:42:49 -0600

    updated request mapping for product endpoints

--- src/main/java/adrian/totallyrealserver/controllers/ProductController.java
+++ src/main/java/adrian/totallyrealserver/controllers/ProductController.java
@@ -7,21 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController
 {
 	@Autowired
 	ProductRepository productRepository;
 
-	@GetMapping("/featured-product")
+	@GetMapping("/featured")
 	public Product getFeaturedProducts()
 	{
 		return productRepository.findById(1L).get();
 	}
 
-	@GetMapping("/product/{id}")
+	@GetMapping("/{id}")
 	public Product getProduct(@PathVariable Long id)
 	{
 		return productRepository.findById(id).get();
